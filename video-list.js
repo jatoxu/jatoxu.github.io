4092480cdb1f544e6919114e0333c7a693d3dcd7
jatoxu <jatosi@163.com>
2026-07-15 23:05:39 +0800

    Add files via upload

---
+++ video-list.js
@@ -0,0 +1,50 @@
+const videoList =[
+  {
+    "vid": "a1b2c3d2",
+    "title": "功夫女足 抢先版",
+    "actor": "张小斐,迪丽热巴,张艺兴,刘嘉玲,佐藤健,艾米,雪野,蔡思贝,胡予安,倪好,赵丽娜,欧阳靖,张继聪,欧阳万成,陈旻,李卓媚",
+    "vYear": "2026-07-13",
+    "url": "https://v.gsuus.com/play/bYEyM2Ab/index.m3u8",
+    "cover": "https://img.guangsuimage.com/cover/9875c52ef5d540a78c772457ee1cc70b.jpg"
+  },
+  {
+    "vid": "a1b2c3d3",
+    "title": "变形金刚4：绝迹重生",
+    "actor": "马克·沃尔伯格,斯坦利·图齐,凯尔希·格兰莫,妮可拉·佩尔茨,杰克·莱诺",
+    "vYear": "2014",
+    "url": "https://v.gsuus.com/play/QdJGR09e/index.m3u8",
+    "cover": "https://img.guangsuimage.com/cover/47951a40efc0d2f7da8ff1ecbfde80f4.jpg"
+  },
+  {
+    "vid": "a1b2c3d4",
+    "title": "变形金刚2（普通话版）",
+    "actor": "希亚·拉博夫,梅根·福克斯,乔什·杜哈明,泰瑞斯·吉布森,约翰·特托罗,拉蒙·罗德里格兹,凯文·杜恩",
+    "vYear": "2009",
+    "url": "https://v.gsuus.com/play/PdR27E0d/index.m3u8",
+    "cover": "https://img.guangsuimage.com/cover/01bead0568960c7daec66bb373943955.jpg"
+  },
+  {
+    "vid": "a1b2c3d5",
+    "title": "变形金刚3（普通话版）",
+    "actor": "希亚·拉博夫,罗茜·汉丁顿-惠特莉,乔什·杜哈明,约翰·特托罗,泰瑞斯",
+    "vYear": "2011",
+    "url": "https://hd.ijycnd.com/play/0dNQRzKb/index.m3u8",
+    "cover": "https://image.jinyingimage.com/cover/81f4dab58146e5703095caae110dd856.jpg"
+  },
+  // {
+  //   "vid": "a1b2c3d6",
+  //   "title": "害羞女大学生的出差按摩初体验记",
+  //   "actor": "相原葵,樋口美津叶,向井莉玖",
+  //   "vYear": "2025",
+  //   "url": "https://ckzy3.com/20260626/ZwP1xG4P/index.m3u8",
+  //   "cover": "https://www.ckzy1.com/upload/vod/20260714-1/fd4b09cb5c91b84d42bbc5b0302c6363.jpg"
+  // },
+  {
+    "vid": "a1b2c3d6",
+    "title": "狼战",
+    "actor": "相原葵,樋口美津叶,向井莉玖",
+    "vYear": "2025",
+    "url": "https://vip.ffzy-play10.com/20260615/68578_e951935d/index.m3u8",
+    "cover": "https://www.imgzy360.com:7788/upload/vod/20250126-1/620daee0077831b8525a85d6ae2a7cb4.jpg"
+  },
+]
